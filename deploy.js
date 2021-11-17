9e95f42323c92b88131eb9b19272878af5fc18e3
ashwani1218 <ashwanipandey1218@gmail.com>
2021-11-17 19:19:54 +0530

    Updated Deployment file

---
+++ deploy.js
@@ -0,0 +1,23 @@
+// https://rinkeby.infura.io/v3/43696b7135424441920a4851ff2f0831
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+const provider = new HDWalletProvider(
+  "infant immense decline canvas skirt intact save smile vendor nephew village initial",
+  "https://rinkeby.infura.io/v3/43696b7135424441920a4851ff2f0831"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Gettign the first account's address: ", accounts[0]);
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+  console.log("Contract deployed to :", result.options.address);
+  provider.engine.stop();
+};
+deploy();
