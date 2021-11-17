dd6a200b0770b12e3af436286af5c72d49941d02
ashwani1218 <ashwanipandey1218@gmail.com>
2021-11-17 23:31:47 +0530

    updated to latest Solidity version

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@
 
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-const { interface, bytecode } = require("./compile");
+const { abi, evm } = require("./compile");
 const provider = new HDWalletProvider(
   "infant immense decline canvas skirt intact save smile vendor nephew village initial",
   "https://rinkeby.infura.io/v3/43696b7135424441920a4851ff2f0831"
@@ -14,8 +14,8 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   console.log("Gettign the first account's address: ", accounts[0]);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+  const result = await new web3.eth.Contract(JSON.parse(abi))
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
     .send({ from: accounts[0], gas: "1000000" });
   console.log("Contract deployed to :", result.options.address);
   provider.engine.stop();
